fe8eccb58808f5eb2e4586f5a7900498c42825b5
Eric <liuweiccy@users.noreply.github.com>
2015-03-02 19:13:37 +0800

    fectch change to fetch

--- src/test/mysql.erl
+++ src/test/mysql.erl
@@ -10,16 +10,16 @@
 -author("Administrator").
 
 %% API
--export([start/0, fectch/0, fectch/2, delete_all/1]).
+-export([start/0, fectch/0, fetch/2, delete_all/1]).
 
 start() ->
 	application:start(p1_mysql),
 	p1_mysql:start_link(0, "localhost", "root", "123456", "test"),
 	p1_mysql:connect(1, "localhost", undefined, "root", "123456", "test", true).
 
-fectch() ->
-	fectch(0, "select * from people").
-fectch(Id, Query) ->
+fetch() ->
+	fetch(0, "select * from people").
+fetch(Id, Query) ->
 	p1_mysql:fetch(Id, Query).
 
 delete_all(Table) ->
